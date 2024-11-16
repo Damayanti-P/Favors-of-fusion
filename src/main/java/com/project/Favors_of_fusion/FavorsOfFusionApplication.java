bc029760e9d278a9d29aa707b68e17ad04125f7f
Damayanti-P <ponkshedamayanti@gmail.com>
2024-11-16 18:06:51 +0530

    Initial commit

---
+++ src/main/java/com/project/Favors_of_fusion/FavorsOfFusionApplication.java
@@ -0,0 +1,13 @@
+package com.project.Favors_of_fusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorsOfFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavorsOfFusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Favors_of_fusion/FavorsOfFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Favors_of_fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavorsOfFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
